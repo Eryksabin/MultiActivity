cb987f377833e1d8c337c4009a259fc3a988524b
Zoraris <eryk.sabiniewicz@gmail.com>
2025-01-23 16:11:34 +0100

    Start

--- app/src/main/java/com/example/mymultiactivityapp/SecondActivity.java
+++ app/src/main/java/com/example/mymultiactivityapp/SecondActivity.java
@@ -1,6 +1,8 @@
 package com.example.mymultiactivityapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,12 @@ public class SecondActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView textView = findViewById(R.id.textViewMessage);
+        String message = getIntent().getStringExtra("message");
+        textView.setText(message);
+
+        Button buttonBack = findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(v -> finish());
     }
 }
